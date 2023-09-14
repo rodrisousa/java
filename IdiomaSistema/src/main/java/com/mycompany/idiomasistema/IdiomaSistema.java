88bc65e2b2ee96814c15ef4cdc8989ae3a70091f
Rodrigo Sousa <115681208+rodrisousa@users.noreply.github.com>
2023-09-14 11:02:14 -0300

    aulas concluídas

---
+++ IdiomaSistema/src/main/java/com/mycompany/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.idiomasistema;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ResolucaoTela/src/main/java/com/mycompany/resolucaotela/ResolucaoTela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.resolucaotela;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class ResolucaoTela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
